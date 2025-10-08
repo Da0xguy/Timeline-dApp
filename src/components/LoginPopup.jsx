7579a70a0e18d948df82b2d36b9d1b5c9e00c458
Ay <ayobamioketona@gmail.com>
2025-10-09 00:10:10 +0100

    third xommit

--- src/components/LoginPopup.jsx
+++ src/components/LoginPopup.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
+import { useNavigate } from "react-router-dom";
 
 function LoginPopup({ onClose }) {
+    const navigate = useNavigate();
+    const [Username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if (username === "admin" && password === "1234") {
+        navigate("/admin");
+      } else {
+        setError("Invali Username or password");
+      }
+    };
   return (
     <div className="popup-overlay">
       <div className="popup">
         <h2>Admin Login</h2>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username"
+        value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password"
+        value={password} onChange={(e) => setUsername(e.target.value)} />
         <div className="popup-actions">
           <button onClick={onClose}>Cancel</button>
           <button>Login</button>
         </div>
+        </form>
       </div>
     </div>
   );
